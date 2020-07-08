8377d08c21fb9910d82521ae0028f6fd2b1970e7
LiuXiang <liu767990032@163.com>
2020-07-08 14:51:34 +0800

    commit

--- practice/src/main/java/com/logoxiang/practice/controller/MyController.java
+++ practice/src/main/java/com/logoxiang/practice/controller/MyController.java
@@ -29,6 +29,7 @@ public class MyController {
 
     @GetMapping("getCommonQuestionList")
     public List<Map<String, Object>> getCommonQuestionList() {
+        //777
         List<Map<String, Object>> commonQuestionList = myDao.getCommonQuestionList();
         //8888
         logger.info("获取问题信息列表："+ JSON.toJSONString(commonQuestionList));
